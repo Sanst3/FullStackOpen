219b056da5d7f945cafbfb56c26652e09e5b80c1
Sanst3 <sanst3a@gmail.com>
2021-08-28 17:42:36 +1000

    Updated to prep for Heroku test

---
+++ backend/index.js
@@ -0,0 +1,93 @@
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+app.use(express.json())
+
+morgan.token('body', (request, response) => JSON.stringify(response.locals.person))
+app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
+
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/info', (request, response) => {
+  const count = persons.length
+  const date = new Date()
+
+  response.send(`Phonebook has info for ${count} people<br>${date}`)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+    response.locals.person = person
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
+})
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  if (!body.hasOwnProperty('name') || !body.hasOwnProperty('number')) {
+    return response.status(400).json({
+      error: 'missing properties'
+    })
+  }
+  if (persons.filter(person => person.name === body.name).length > 0) {
+    return response.status(400).json({
+      error: 'duplicate name'
+    })
+  }
+
+  const person = {
+    "id": getRandomInt(30),
+    "name": body.name,
+    "number": body.number
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
+})
+
+const PORT = proces.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
