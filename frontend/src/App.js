b43893d78d7ca173f8f10bbc75707be7e3d6d1a0
Sanst3 <sanst3a@gmail.com>
2021-08-31 14:07:21 +1000

    uibuild

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -43,13 +43,13 @@ const App = () => {
     setNewQuery(e.target.value)
   }
 
-  useEffect(() => {
-    axios
-    .get(`/api/persons`)
-    .then(response => {
-      setPersons(response.data)
-    })
-  }, [])
+  // useEffect(() => {
+  //   axios
+  //   .get(`/api/persons`)
+  //   .then(response => {
+  //     setPersons(response.data)
+  //   })
+  // }, [])
 
 
   return (
